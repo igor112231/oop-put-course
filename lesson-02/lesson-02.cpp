26936a5cd63c06c9cc5c6dbea02901c8d0bcef9d
igor112231 <127102107+igor112231@users.noreply.github.com>
2023-04-11 21:28:30 +0200

    Add files via upload

---
+++ lesson-02/lesson-02.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ryba
+{
+public:
+    string gatunek;
+    bool slodkowodna;
+    int ilosc_lusek;
+};
+
+class Ptak
+{
+public:
+    string gatunek;
+    string ulubiona_ryba;
+    int ilosc_pior;
+};
+
+class Kot
+{
+public:
+    string ulubiona_ryba;
+    string ulubiony_ptak;
+    int ilosc_zyc;
+};
+
+int main()
+{
+    Ryba nemo;
+    nemo.gatunek = "blazenek";
+    nemo.slodkowodna = 0;
+    nemo.ilosc_lusek = 99;
+
+    Ptak simba;
+    simba.gatunek = "orzel";
+    simba.ulubiona_ryba = "nemo";
+    simba.ilosc_pior = 999;
+
+    Kot w_butach;
+    w_butach.ulubiona_ryba = "nemo";
+    w_butach.ulubiony_ptak = "simba";
+    w_butach.ilosc_zyc = 9;
+
+}
+
